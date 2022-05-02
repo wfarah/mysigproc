7d9c50ee24323f8ebaf9b3eccf772c08b1af12ea
sonata <wael.a.farah@gmail.com>
2022-05-02 23:38:46 +0000

    Fix compile-time warnings

--- mysigproc_utils.c
+++ mysigproc_utils.c
@@ -16,12 +16,13 @@ void unpack_2bit(unsigned char, float*);
 void get_string(FILE *inputfile, int *nbytes, char string[]) /* includefile */
 {
   int nchar;
+  size_t fread_sz;
   strcpy(string,"ERROR");
-  fread(&nchar, sizeof(int), 1, inputfile);
+  fread_sz = fread(&nchar, sizeof(int), 1, inputfile);
   *nbytes=sizeof(int);
   if (feof(inputfile)) exit(0);
   if (nchar>80 || nchar<1) return;
-  fread(string, nchar, 1, inputfile);
+  fread_sz = fread(string, nchar, 1, inputfile);
   string[nchar]='\0';
   *nbytes+=nchar;
 }
@@ -31,12 +32,13 @@ void get_string(FILE *inputfile, int *nbytes, char string[]) /* includefile */
 int get_header(char *fil_direc, sigproc_header_t* header) /* includefile */
 {
   //struct sigproc_header_t header;
-  char string[80], message[80];
+  char string[80], message[256];
   header->expecting_rawdatafile=0;
   header->expecting_source_name=0; 
   header->expecting_frequency_table=0;
   int nbytes;
   long int sz;
+  size_t fread_sz;
   strcpy(header->file_name,fil_direc);
   FILE * inputfile = fopen(fil_direc,"rb");
   if (inputfile==NULL){
@@ -68,78 +70,83 @@ int get_header(char *fil_direc, sigproc_header_t* header) /* includefile */
     } else if (strcmp(string,"FREQUENCY_END")==0) {
       header->expecting_frequency_table=0;
     } else if (strcmp(string,"az_start")==0) {
-      fread(&header->az_start,sizeof(header->az_start),1,inputfile);
+      fread_sz = fread(&header->az_start,sizeof(header->az_start),1,inputfile);
       header->totalbytes+=sizeof(header->az_start);
     } else if (strcmp(string,"za_start")==0) {
-      fread(&header->za_start,sizeof(header->za_start),1,inputfile);
+      fread_sz = fread(&header->za_start,sizeof(header->za_start),1,inputfile);
       header->totalbytes+=sizeof(header->za_start);
     } else if (strcmp(string,"src_raj")==0) {
-      fread(&header->src_raj,sizeof(header->src_raj),1,inputfile);
+      fread_sz = fread(&header->src_raj,sizeof(header->src_raj),1,inputfile);
       header->totalbytes+=sizeof(header->src_raj);
     } else if (strcmp(string,"src_dej")==0) {
-      fread(&header->src_dej,sizeof(header->src_dej),1,inputfile);
+      fread_sz = fread(&header->src_dej,sizeof(header->src_dej),1,inputfile);
       header->totalbytes+=sizeof(header->src_dej);
     } else if (strcmp(string,"tstart")==0) {
-      fread(&header->tstart,sizeof(header->tstart),1,inputfile);
+      fread_sz = fread(&header->tstart,sizeof(header->tstart),1,inputfile);
       header->totalbytes+=sizeof(header->tstart);
     } else if (strcmp(string,"tsamp")==0) {
-      fread(&header->tsamp,sizeof(header->tsamp),1,inputfile);
+      fread_sz = fread(&header->tsamp,sizeof(header->tsamp),1,inputfile);
       header->totalbytes+=sizeof(header->tsamp);
     } else if (strcmp(string,"period")==0) {
-      fread(&header->period,sizeof(header->period),1,inputfile);
+      fread_sz = fread(&header->period,sizeof(header->period),1,inputfile);
       header->totalbytes+=sizeof(header->period);
     } else if (strcmp(string,"fch1")==0) {
-      fread(&header->fch1,sizeof(header->fch1),1,inputfile);
+      fread_sz = fread(&header->fch1,sizeof(header->fch1),1,inputfile);
       header->totalbytes+=sizeof(header->fch1);
     } else if (strcmp(string,"fchannel")==0) {
-      fread(&header->frequency_table[header->channel_index++],sizeof(double),1,inputfile);
+      fread_sz = fread(&header->frequency_table[header->channel_index++],sizeof(double),1,inputfile);
       header->totalbytes+=sizeof(double);
       header->fch1=header->foff=0.0; /* set to 0.0 to signify that a table is in use */
     } else if (strcmp(string,"foff")==0) {
-      fread(&header->foff,sizeof(header->foff),1,inputfile);
+      fread_sz = fread(&header->foff,sizeof(header->foff),1,inputfile);
       header->totalbytes+=sizeof(header->foff);
     } else if (strcmp(string,"nchans")==0) {
-      fread(&header->nchans,sizeof(header->nchans),1,inputfile);
+      fread_sz = fread(&header->nchans,sizeof(header->nchans),1,inputfile);
       header->totalbytes+=sizeof(header->nchans);
     } else if (strcmp(string,"telescope_id")==0) {
-      fread(&header->telescope_id,sizeof(header->telescope_id),1,inputfile);
+      fread_sz = fread(&header->telescope_id,sizeof(header->telescope_id),1,
+          inputfile);
       header->totalbytes+=sizeof(header->telescope_id);
     } else if (strcmp(string,"machine_id")==0) {
-      fread(&header->machine_id,sizeof(header->machine_id),1,inputfile);
+      fread_sz = fread(&header->machine_id,sizeof(header->machine_id),1,
+          inputfile);
       header->totalbytes+=sizeof(header->machine_id);
     } else if (strcmp(string,"data_type")==0) {
-      fread(&header->data_type,sizeof(header->data_type),1,inputfile);
+      fread_sz = fread(&header->data_type,sizeof(header->data_type),1,
+          inputfile);
       header->totalbytes+=sizeof(header->data_type);
     } else if (strcmp(string,"ibeam")==0) {
-      fread(&header->ibeam,sizeof(header->ibeam),1,inputfile);
+      fread_sz = fread(&header->ibeam,sizeof(header->ibeam),1,inputfile);
       header->totalbytes+=sizeof(header->ibeam);
     } else if (strcmp(string,"nbeams")==0) {
-      fread(&header->nbeams,sizeof(header->nbeams),1,inputfile);
+      fread_sz = fread(&header->nbeams,sizeof(header->nbeams),1,inputfile);
       header->totalbytes+=sizeof(header->nbeams);
     } else if (strcmp(string,"nbits")==0) {
-      fread(&header->nbits,sizeof(header->nbits),1,inputfile);
+      fread_sz = fread(&header->nbits,sizeof(header->nbits),1,inputfile);
       header->totalbytes+=sizeof(header->nbits);
     } else if (strcmp(string,"barycentric")==0) {
-      fread(&header->barycentric,sizeof(header->barycentric),1,inputfile);
+      fread_sz = fread(&header->barycentric,sizeof(header->barycentric),1,
+          inputfile);
       header->totalbytes+=sizeof(header->barycentric);
     } else if (strcmp(string,"pulsarcentric")==0) {
-      fread(&header->pulsarcentric,sizeof(header->pulsarcentric),1,inputfile);
+      fread_sz = fread(&header->pulsarcentric,sizeof(header->pulsarcentric),1,
+          inputfile);
       header->totalbytes+=sizeof(header->pulsarcentric);
     } else if (strcmp(string,"nbins")==0) {
-      fread(&header->nbins,sizeof(header->nbins),1,inputfile);
+      fread_sz = fread(&header->nbins,sizeof(header->nbins),1,inputfile);
       header->totalbytes+=sizeof(header->nbins);
     } else if (strcmp(string,"nsamples")==0) {
       /* read this one only for backwards compatibility */
-      fread(&header->itmp,sizeof(header->itmp),1,inputfile);
+      fread_sz = fread(&header->itmp,sizeof(header->itmp),1,inputfile);
       header->totalbytes+=sizeof(header->itmp);
     } else if (strcmp(string,"nifs")==0) {
-      fread(&header->nifs,sizeof(header->nifs),1,inputfile);
+      fread_sz = fread(&header->nifs,sizeof(header->nifs),1,inputfile);
       header->totalbytes+=sizeof(header->nifs);
     } else if (strcmp(string,"npuls")==0) {
-      fread(&header->npuls,sizeof(header->npuls),1,inputfile);
+      fread_sz = fread(&header->npuls,sizeof(header->npuls),1,inputfile);
       header->totalbytes+=sizeof(header->npuls);
     } else if (strcmp(string,"refdm")==0) {
-      fread(&header->refdm,sizeof(header->refdm),1,inputfile);
+      fread_sz = fread(&header->refdm,sizeof(header->refdm),1,inputfile);
       header->totalbytes+=sizeof(header->refdm);
     } else if (header->expecting_rawdatafile) {
       strcpy(header->rawdatafile,string);
@@ -287,6 +294,7 @@ int read_block_TF(uint64_t sample, uint64_t nsamples, fil_t* filterbank,
 int read_block(uint64_t start_sample, uint64_t nsamples, fil_t* filterbank, 
     const int order, float * buff)
 {
+  size_t fread_sz;
   if (start_sample+nsamples > filterbank->header->nsamples)
   {
     fprintf(stderr,
@@ -331,7 +339,8 @@ int read_block(uint64_t start_sample, uint64_t nsamples, fil_t* filterbank,
 
     case 32:
       if (order == SIGPROC_ORDER_TF)
-        fread(buff, 4, filterbank->header->nchans*nsamples, filterbank->file);
+        fread_sz = fread(buff, 4, 
+            filterbank->header->nchans*nsamples, filterbank->file);
 
       else if (order == SIGPROC_ORDER_FT)
       {
@@ -339,7 +348,7 @@ int read_block(uint64_t start_sample, uint64_t nsamples, fil_t* filterbank,
         {
           for(chan=(filterbank->header->nchans -1); chan>=0; chan--)
           {
-            fread(&buff[chan*nsamples+i], 4, 1, filterbank->file);
+            fread_sz = fread(&buff[chan*nsamples+i], 4, 1, filterbank->file);
           }
         }
       }
@@ -362,6 +371,7 @@ int read_block(uint64_t start_sample, uint64_t nsamples, fil_t* filterbank,
 int read_block_rescale(uint64_t start_sample, uint64_t nsamples, fil_t* filterbank, 
     const int order, float * buff, float mean, float stdev)
 {
+  size_t fread_sz;
   if (start_sample+nsamples > filterbank->header->nsamples)
   {
     fprintf(stderr,
@@ -453,7 +463,8 @@ int read_block_rescale(uint64_t start_sample, uint64_t nsamples, fil_t* filterba
     case 32:
       if (order == SIGPROC_ORDER_TF)
       {
-        fread(buff, 4, filterbank->header->nchans*nsamples, filterbank->file);
+        fread_sz = fread(buff, 4, filterbank->header->nchans*nsamples, 
+            filterbank->file);
         for (i=0; i<filterbank->header->nchans*nsamples; i++)
           buff[i] = (buff[i] - mean)/stdev;
       }
@@ -464,7 +475,7 @@ int read_block_rescale(uint64_t start_sample, uint64_t nsamples, fil_t* filterba
 //          for(chan=0;chan<filterbank->header->nchans;chan++)
           for(chan=(filterbank->header->nchans - 1); chan>=0; chan--)
           {
-            fread(&buff[chan*nsamples+i], 4, 1, filterbank->file);
+            fread_sz = fread(&buff[chan*nsamples+i], 4, 1, filterbank->file);
             buff[chan*nsamples+i] = (buff[chan*nsamples+i] - mean)/stdev;
           }
         }
@@ -487,6 +498,7 @@ int read_block_rescale(uint64_t start_sample, uint64_t nsamples, fil_t* filterba
 int read_block_rescale_inv_chan(uint64_t start_sample, uint64_t nsamples, fil_t* filterbank, 
     const int order, float * buff, float mean, float stdev)
 {
+  size_t fread_sz;
   if (start_sample+nsamples > filterbank->header->nsamples)
   {
     fprintf(stderr,
@@ -540,7 +552,8 @@ int read_block_rescale_inv_chan(uint64_t start_sample, uint64_t nsamples, fil_t*
         {
           for(chan=(filterbank->header->nchans - 1);chan>=0;chan--)
           {
-            fread(&buff[i*filterbank->header->nchans+chan], 4, 1, filterbank->file);
+            fread_sz = fread(&buff[i*filterbank->header->nchans+chan], 
+                4, 1, filterbank->file);
             buff[i*filterbank->header->nchans+chan] = (buff[i*filterbank->header->nchans+chan] - mean)/stdev;
           }
         }
@@ -554,7 +567,7 @@ int read_block_rescale_inv_chan(uint64_t start_sample, uint64_t nsamples, fil_t*
         {
           for(chan=(filterbank->header->nchans -1); chan>=0; chan--)
           {
-            fread(&buff[chan*nsamples+i], 4, 1, filterbank->file);
+            fread_sz = fread(&buff[chan*nsamples+i], 4, 1, filterbank->file);
             buff[chan*nsamples+i] = (buff[chan*nsamples+i] - mean)/stdev;
           }
         }
@@ -575,7 +588,7 @@ int read_block_rescale_inv_chan(uint64_t start_sample, uint64_t nsamples, fil_t*
           {
             if (bit_count >= 4)
             {
-              fread(&tmp, 1, 1, filterbank->file);
+              fread_sz = fread(&tmp, 1, 1, filterbank->file);
               unpack_2bit(tmp, tmp_2bit);
               bit_count = 0;
             }
@@ -592,7 +605,7 @@ int read_block_rescale_inv_chan(uint64_t start_sample, uint64_t nsamples, fil_t*
           {
             if (bit_count >= 4)
             {
-              fread(&tmp, 1, 1, filterbank->file);
+              fread_sz = fread(&tmp, 1, 1, filterbank->file);
               unpack_2bit(tmp, tmp_2bit);
               bit_count = 0;
             }
